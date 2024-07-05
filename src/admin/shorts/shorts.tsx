f0b679dc2060c8381fe8489bf9a4e58ec2423cc3
nirajsathe80 <nirajsathe80@gmail.com>
2024-07-05 17:13:41 +0530

    unused code remove fro, short section

--- src/admin/shorts/shorts.tsx
+++ src/admin/shorts/shorts.tsx
@@ -6,48 +6,47 @@ import Reflection from "./reflection";
 import { shortsFormService } from "@/src/service/shorts.service";
 
 const ShortsAdmin = () => {
-    const [inputValues, setInputValues] = useState({
-        value1: "",
-        value2: ""
-    });
+  const [inputValues, setInputValues] = useState({
+    value1: "",
+    value2: "",
+  });
+  const { value1, value2 } = inputValues;
 
-    const handleInputChange = (e: any) => {
-        setInputValues({
-            ...inputValues,
-            value1: e.target.value
-        });
-    };
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const id = e.target.id;
+    const value = e.target.value;
+    setInputValues((prev) => ({ ...prev, [id]: value }));
+  };
 
-    const handleInputChange2 = (e: any) => {
-        setInputValues({
-            ...inputValues,
-            value2: e.target.value
-        });
-    };
+  const handleOnSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      if (value1.length > 0 && value2.length > 0) {
+        const shortsFormResponse = await shortsFormService.shortsFormData(
+          JSON.stringify(inputValues)
+        );
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  };
 
-    const handleOnSubmit = async(e: any) => {
-        e.preventDefault();
-        try {
-            if (inputValues.value1.length > 0 && inputValues.value2.length > 0) {
-                const shortsFormResponse = await shortsFormService.shortsFormData(JSON.stringify(inputValues));
-                console.log('true', shortsFormResponse);
-            }
-        } catch (error) {
-            console.error('Error submitting form:', error);
-        }
-    };
-
-    return (
-        <div className="m-10">
-            <form onSubmit={handleOnSubmit}>
-                <InputField value={inputValues.value1} onChange={handleInputChange} />
-                <InputField value={inputValues.value2} onChange={handleInputChange2} />
-                <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md">Submit</button>
-            </form>
-            <Reflection value={inputValues.value1} />
-            <Reflection value={inputValues.value2} />
-        </div>
-    );
+  return (
+    <div className="m-10">
+      <form onSubmit={handleOnSubmit}>
+        <InputField value={value1} onChange={handleInputChange} id="value1" />
+        <InputField value={value2} onChange={handleInputChange} id="value2" />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white py-2 px-4 rounded-md"
+        >
+          Submit
+        </button>
+      </form>
+      <Reflection value={value1} />
+      <Reflection value={value2} />
+    </div>
+  );
 };
 
 export default ShortsAdmin;
