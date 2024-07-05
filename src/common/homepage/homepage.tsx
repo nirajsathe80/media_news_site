de72b16c7312e9d453c607b18b35cd5272feaf5d
nirajsathe80 <nirajsathe80@gmail.com>
2024-07-05 17:17:59 +0530

    homepage section modified

--- src/common/homepage/homepage.tsx
+++ src/common/homepage/homepage.tsx
@@ -6,8 +6,6 @@ import ShortTrending from "@/src/components/short-trending/shortTrending";
 import Slide from "@/src/components/slide/slide";
 import Timeline from "@/src/components/timeline/timeline";
 import TopStories from "@/src/components/top-stories/topStories";
-import SpeedDial from "../speed-dial/speed-dial";
-import { TwitterTweetEmbed } from "react-twitter-embed";
 
 const HomePage = () => {
   return (
@@ -15,24 +13,9 @@ const HomePage = () => {
       <Slide />
       <LatestArticles />
       <ShortTrending />
-      {/* <div className="mx-[40px]">
-        <TwitterTweetEmbed
-          tweetId={"1713207336813121836"}
-          options={{
-            align: "left",
-          }}
-        />
-        <TwitterTweetEmbed
-          tweetId={"1713026071929696757"}
-          options={{
-            align: "right",
-          }}
-        />
-      </div> */}
       <TopStories />
       <Timeline />
       <RelatedArticles />
-      <SpeedDial />
     </>
   );
 };
