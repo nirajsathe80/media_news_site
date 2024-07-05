459c965e86a7208528680d39cfcf87136ff34c38
nirajsathe80 <nirajsathe80@gmail.com>
2024-07-05 17:13:12 +0530

    input field component type protected

--- src/admin/shorts/input-field.tsx
+++ src/admin/shorts/input-field.tsx
@@ -1,7 +1,13 @@
 // InputField.js
 import React from "react";
 
-const InputField = ({ value, onChange }: any) => {
+interface InputFieldProps {
+  value: any;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  id?: string;
+}
+
+const InputField = ({ value, onChange, id }: InputFieldProps) => {
   return (
     <>
       <input
@@ -10,8 +16,8 @@ const InputField = ({ value, onChange }: any) => {
         onChange={onChange}
         placeholder="Type something"
         className="w-full my-10 mr-2 border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+        id={id}
       />
-     
     </>
   );
 };
