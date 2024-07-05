e882bc2feb382174879d29741f444a420d66df25
nirajsathe80 <nirajsathe80@gmail.com>
2024-07-05 17:18:46 +0530

    common data file add

---
+++ src/components/data/index.ts
@@ -0,0 +1,108 @@
+import tuturialSvg from "../../../public/tutorial.svg";
+import articleSvg from "../../../public/article.svg";
+
+export const accordionData = [
+  {
+    title: "Breaking: Major Tech Company Unveils Revolutionary AI Technology",
+    content:
+      "In a groundbreaking announcement, a leading tech company revealed its latest AI technology that promises to reshape industries. Experts believe this innovation will pave the way for a new era in artificial intelligence, transforming how businesses operate and revolutionizing everyday life.",
+  },
+  {
+    title:
+      "Global Climate Summit: World Leaders Unite to Combat Climate Change",
+    content:
+      "Leaders from around the world gathered at the Global Climate Summit to address the pressing issue of climate change. Committing to ambitious goals, nations pledged to reduce carbon emissions, invest in renewable energy, and implement sustainable practices. The summit marks a significant step toward a greener and more sustainable future.",
+  },
+  {
+    title: "Space Exploration Milestone: Astronauts Successfully Land on Mars",
+    content:
+      "In an historic achievement, astronauts landed safely on Mars after a months-long journey. This monumental feat opens new horizons for humanity's exploration of space, offering valuable insights into the Martian terrain and potential for future colonization. Scientists and space enthusiasts worldwide celebrate this remarkable accomplishment.",
+  },
+];
+
+export const relatedArticleData = [
+  {
+    svg: tuturialSvg,
+    category: "Tutorial",
+    publised: "14 days ago",
+    heading: "How to quickly deploy a static website",
+    disc: `Static websites are now used to bootstrap lots of websites and are
+          becoming the basis for a variety of tools that even influence both
+          web designers and developers influence both web designers and
+          developers`,
+    artistImg:
+      "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
+    artistName: "Jese Leos",
+  },
+  {
+    svg: articleSvg,
+    category: "Article",
+    publised: "14 days ago",
+    heading: "Our first project with React",
+    disc: `Static websites are now used to bootstrap lots of websites and are
+              becoming the basis for a variety of tools that even influence both
+              web designers and developers influence both web designers and
+              developers.`,
+    artistImg:
+      "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png",
+    artistName: "Bonnie Green",
+  },
+];
+
+export const faqData = [
+  {
+    title: ` What do you mean by "Figma assets"?`,
+    disc: ` You will have access to download the full Figma project
+            including all of the pages, the components, responsive pages,
+            and also the icons, illustrations, and images included in the
+            screens.`,
+  },
+  {
+    title: `  What does"lifetime access" exactly mean?`,
+    disc: `  Once you have purchased either the design, code, or both
+                  packages, you will have access to all of the future updates
+                  based on the roadmap, free of charge.`,
+  },
+  {
+    title: ` How does support work?`,
+    disc: ` We're aware of the importance of well qualified support,
+                  that is why we decided that support will only be provided by
+                  the authors that actually worked on this project.`,
+  },
+  {
+    title: ` I want to build more than one project. Is that allowed?`,
+    disc: `  You can use Windster for an unlimited amount of projects,
+                  whether it's a personal website, a SaaS app, or a website
+                  for a client. As long as you don't build a product that
+                  will directly compete with Windster either as a UI kit, theme,
+                  or template, it's fine.`,
+  },
+  {
+    title: ` What does "free updates" include?`,
+    disc: ` The free updates that will be provided is based on th
+             that we have laid out for this project. It is also possible
+          that we will provide extra updates outside of the roadmap as
+              well.`,
+  },
+  {
+    title: ` What does the free version include?`,
+    disc: ` The free version
+            of Windster includes a minimal style guidelines, component
+                  variants, and a dashboard page with the mobile version
+                  alongside it.`,
+  },
+  {
+    title: ` What is the difference between Windster and Tailwind UI?`,
+    disc: `Although both Windster and Tailwind UI are built for
+                  integration with Tailwind CSS, the main difference is in the
+                  design, the pages, the extra components and UI elements that
+                  Windster includes.`,
+  },
+  {
+    title: `Can I use Windster in open-source projects?`,
+    disc: `  Generally, it is accepted to use Windster in open-source
+                  projects, as long as it is not a UI library, a theme, a
+                  template, a page-builder that would be considered as an
+                  alternative to Windster itself.`,
+  },
+];
