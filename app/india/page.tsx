a78feb9874c9e5be4ec5b8746b09321e28edfaa5
nirajsathe80 <nirajsathe80@gmail.com>
2024-07-05 17:10:43 +0530

    Feat: india section modified

--- app/india/page.tsx
+++ app/india/page.tsx
@@ -1,15 +1,20 @@
 "use client";
 import Footer from "@/src/common/footer";
 import Header from "@/src/common/header";
-import SpeedDial from "@/src/common/speed-dial/speed-dial";
+import PageStructure from "@/src/common/page-structure";
 import India from "@/src/components/India/india";
+import IndiaNews from "../../src/components/India/indiaNewsJson/india-news.json";
+import Pagination from "@/src/components/pagination/pagination";
+import NewsLetter from "@/src/common/news-letter/newsLetter";
 
 const IndiaPage = () => {
+  const { news } = IndiaNews;
   return (
     <div>
       <Header />
-      <India />
-      <SpeedDial />
+      <PageStructure pageTitle="India" newsArray={news} />
+      <Pagination />
+      <NewsLetter />
       <Footer />
     </div>
   );
