a6dbdb6a1347693a400b7f67454ed3ab5e24ebdf
nirajsathe80 <nirajsathe80@gmail.com>
2024-07-07 21:55:50 +0530

    category error fix

--- src/common/page-structure/index.tsx
+++ src/common/page-structure/index.tsx
@@ -29,14 +29,20 @@ const PageStructure = ({ pageTitle, newsArray }: PageStructureProps) => {
                   <img src={src} className="w-full max-h-60" alt="Louvre" />
                   <Link
                     href={`/${category}/[id]`}
-                    as={`/${category}/${dynamicId}`}
+                    as={`/${category.toLowerCase()}/${dynamicId}`}
                     className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100 bg-[hsla(0,0%,98.4%,.15)]"
                   />
                 </div>
               </div>
 
               <div className="mb-6 mr-auto w-full shrink-0 grow-0 basis-auto px-3 md:mb-0 md:w-9/12 xl:w-7/12">
-                <h5 className="mb-3 text-lg font-bold">{title}</h5>
+                <Link
+                  href={`/${category}/[id]`}
+                  as={`/${category.toLowerCase()}/${dynamicId}`}
+                  className="mb-3 text-lg font-bold cursor-pointer"
+                >
+                  {title}
+                </Link>
                 <div className="mb-3 flex items-center justify-center text-sm font-medium text-danger md:justify-start">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
