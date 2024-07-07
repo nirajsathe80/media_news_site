5e8151ec31c4c0ab74899a1bc086abb9134fcb02
nirajsathe80 <nirajsathe80@gmail.com>
2024-07-07 22:00:36 +0530

    extra code removed

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import BlogPageCommonStructure from "@/src/common/blog-page-structure";
-import SpeedDial from "@/src/common/speed-dial/speed-dial";
 
 const BlogInformation = () => {
   return (
     <>
       <BlogPageCommonStructure />
-      <SpeedDial />
     </>
   );
 };
