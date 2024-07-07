9147251b13fcafcc562d5593d8dec1d142781e3c
nirajsathe80 <nirajsathe80@gmail.com>
2024-07-07 22:00:48 +0530

    lifestyle id page created

---
+++ app/lifestyle/[id]/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import BlogPageCommonStructure from "@/src/common/blog-page-structure";
+
+const LifeStyleDetails = () => {
+  return (
+    <>
+      <BlogPageCommonStructure />
+    </>
+  );
+};
+
+export default LifeStyleDetails;
